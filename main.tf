0ed2b890d61449d19c4598a852d42ea8b80d1a5e
haryoiro <mizusecocolte@gmail.com>
2023-06-24 12:22:27 +0900

    🚑 間違えた記述を修正

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                   = "ap-northeast-1"
-  shared_credentials_files = "[$HOME/.aws/credentials]"
+  shared_credentials_files = ["$HOME/.aws/credentials"]
 }
 
 # 自分のパブリックIP取得用
