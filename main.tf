40045d4e1ca7c673a1aead65d192ec2966b75cbb
haryoiro <mizusecocolte@gmail.com>
2023-06-25 20:46:52 +0900

    👍 pokbun provider追加

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    porkbun = {
+      source = "cullenmcdermott/porkbun"
+      version = "0.2.0"
+    }
+  }
+}
+
 provider "aws" {
   region                   = "ap-northeast-1"
   shared_credentials_files = ["$HOME/.aws/credentials"]
@@ -5,3 +14,7 @@ provider "aws" {
 
 # 自分のパブリックIP取得用
 provider "http" {}
+
+provider "porkbun" {
+  api_key = local.keys["API_KEY"]
+}
